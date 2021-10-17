ff272fc12ebde9ca8c7f474b8c205f7ef1f4000b
Salman Abbas <salman6abbas@gmail.com>
2021-10-17 20:33:02 +0500

    UPDATED new changes

--- src/App.js
+++ src/App.js
@@ -7,22 +7,22 @@ import AuthContext from './auth-context'
 
 const App = props => {
   const [page, setPage] = useState('auth')
-  const [authStatus, setAuthStatus]= useState(false);
-  const switchPage = (pageName) =>{
+  const [authStatus, setAuthStatus] = useState(false);
+  const switchPage = (pageName) => {
     setPage(pageName)
   }
 
-  const login = () =>{
+const login = () => {
     setAuthStatus(true)
-  }
-
+}
+  
   return (
     <div className="App">
-      <AuthContext.Provider value = {{status: authStatus, login: login}}>
-    <Header onLoadTodos={switchPage.bind(this, 'todos')} onLoadAuth={switchPage.bind(this, 'auth')}/>
-    <hr />
-    {page=== 'auth'?  <Auth /> : <Todo />}
-    </AuthContext.Provider>
+      <AuthContext.Provider value={{ status: authStatus, login: login }}>
+        <Header onLoadTodos={switchPage.bind(this, 'todos')} onLoadAuth={switchPage.bind(this, 'auth')} />
+        <hr />
+        {page === 'auth' ? <Auth /> : <Todo />}
+      </AuthContext.Provider>
     </div>
   );
 }
